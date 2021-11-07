869efb90764f3fa57345cee56aa7971d58436f14
Jacob Waters <fierywaters13@gmail.com>
2021-11-07 15:50:58 -0800

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 // -------------------------------------------------------------
-// CAN Read Speed test
+// Hello World CAN send test
 // by Jacob Waters
 //
-// This is a simple speed test for receiving CAN packets on a teensy 3.6
+// This test transmits a sample CAN frame over teensy CAN0
 // teensy pin 3 -> CAN TX
 // teensy pin 4 _> CAN RX
 // CANL -> CANL
 // CANH -> CANH
 // GND -> GND I.E. connect grounds of CAN Nodes
 
-#include <Arduino.h>
 #include <FlexCAN.h>
-#include <Streaming.h>
+
+#ifndef __MK66FX1M0__
+#error "Teensy 3.6 with dual CAN bus is required to run this example"
+#endif
 
 int busSpeed = 500000; //baudrate
 bool canBus = 0;       //use 0 CAN0 or 1 for CAN1
@@ -19,72 +21,43 @@ FlexCAN CANbus0(busSpeed, canBus);
 
 static CAN_message_t msg;
 
-uint32_t canCount = 0;
-uint32_t startTime, lastReceivedTime;
 // -------------------------------------------------------------
 void setup(void)
 {
-  while (!Serial)
-    ; //wait for serial to connect
-  CANbus0.begin();
+    CANbus0.begin();
 
-  Serial.println(F("Teensy 3.6 can bus Speed test READ"));
+    while (!Serial)
+        ; //wait for serial bus to be available
 
-  startTime = millis();
-  lastReceivedTime = millis();
+    Serial.println(F("Teensy CAN Speed test WRITE"));
+
+    msg.ext = 1;
+    msg.id = 0x666; //will overflow to 0 in regular ID mode,
+                   //but not in extended ID Mode
+
+    msg.timeout = 1000; //How long to keep trying before message failure in milliseconds
+
+    msg.len = 8; //length in bytes of message, up to 8 bytes
+   for (int i = 0; i < msg.len; i++)
+    {
+      msg.buf[i] = i;
+    }
 }
 
-// -------------------------------------------------------------
-void printData(CAN_message_t &msg)
-{
-  for (int i = 0; i < msg.len - 1; i++)
-  {
-    Serial.print(msg.buf[i], HEX);
-    Serial.print(".");
-  }
-  Serial.print(msg.buf[msg.len - 1], HEX);
-  Serial.println();
-}
 // -------------------------------------------------------------
 void loop(void)
 {
-  //delay(1000);
-  //Serial.println(CANbus0.available());
-  if (millis() - lastReceivedTime > 100)
-  {
-    if (canCount > 0)
+    bool writeSuccessful;
+    for (int i = 0; i < 50000; i++)
     {
-      double secondsElapsed = (millis() - startTime) / 1000.0;
-      Serial << "canCount: " << canCount << endl;
-      Serial << "seconds: " << secondsElapsed << endl;
-      double framesPerSecond = canCount / secondsElapsed;
-      Serial << "Frames/S: " << framesPerSecond << endl;
-      Serial << "effectiveBitrate: " << (64+31)*framesPerSecond << endl << endl;
-      Serial << "Teensy 3.6 can bus SPEED test" << endl;
-
-
-      canCount = 0;
+        //if(CANbus0.available())
+        //{
+        writeSuccessful = CANbus0.write(msg);
+        if (!writeSuccessful)
+            Serial.println("Write failed, check hardware config");
+        //}
     }
-
-    startTime = millis();
-  }
-
-  if (CANbus0.available())
-  {
-    CANbus0.read(msg);
-    lastReceivedTime = millis();
-    canCount++;
-    //Serial.print("CAN bus 0: ");
-    //printData(msg);
-    if(msg.id != 0x666)
-      Serial << "ERROR, ID Doesn't match" << endl;
-    bool copyError = false;
-    for(int i =0; i<msg.len;i++)
-      if(msg.buf[i]!=i)
-        copyError = true;
-    if(copyError)
-      Serial << "ERROR IN DATA" << endl;
-    //Serial.print("ID: ");
-    //Serial.println(msg.id, HEX);
-  }
+    
+    while (1) //wait forever
+        delay(1000);
 }
